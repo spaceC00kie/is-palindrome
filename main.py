4b68862030556d4f69b9cc084570b2c670626b1f
spaceC00kie <kirstie317@gmail.com>
2021-05-05 23:38:24 -0400

    Finished and tested

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # given a string, write a function that returns a boolean saying if the string is a palindrome or not
 
-def is_palindrome(str):
-    # make string into array
+def is_palindrome(word):
+    # copy string into array
+    arr = list(word)
     # define left and right pointer variables
-    # while loop moving pointers in until they cross
+    left = 0
+    right = len(arr) - 1
+    # while pointers haven't crossed
+    while left < right:
         # check if values are same
-        # if values are equal return "true"
+        # if values are equal do nothing
         # if values are not equal return "false"
-        # move both pointers in
+        if arr[left] != arr[right]:
+            return False
+        left += 1
+        right -= 1
+    return True
+
+
+print(is_palindrome("kayak"))
